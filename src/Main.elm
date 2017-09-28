77f2f660d03444c74484648f595fe460584e4909
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2017-09-28 18:06:19 +0200

    Create a type alias for the flags

--- src/Main.elm
+++ src/Main.elm
@@ -12,7 +12,7 @@ import View
 -}
 
 
-main : Program { hash : String } Model.Model Update.Msg
+main : Program Flags Model.Model Update.Msg
 main =
     Html.programWithFlags
         { init = (.hash >> Model.decode)
@@ -20,3 +20,13 @@ main =
         , view = View.view
         , subscriptions = Subs.subscriptions
         }
+
+
+{-|
+
+    This is the type of the object that will be passed to the app from javascript on initialization.
+    The field `hash` will contain the hash part of the URL without the leading hash character.
+-}
+type alias Flags =
+    { hash : String
+    }
